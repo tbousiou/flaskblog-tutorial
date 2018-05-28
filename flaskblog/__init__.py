67a24adcb976901c5015ba019cec4eec9595bc03
Theodoros Bousios <tbousiou@gmail.com>
2018-05-28 23:55:58 +0300

    email and password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,16 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '34534543345ABACAA32434'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'ef9cd3b392bece'
+app.config['MAIL_PASSWORD'] = 'a802660defa5a0'
+
+mail = Mail(app)
 
 from flaskblog import routes
